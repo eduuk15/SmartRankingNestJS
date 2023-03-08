667788b51469fe0601c25a74f1823db86d4b1dcb
Eduardo <eduardo@conectra.com.br>
2023-03-08 10:55:16 -0300

    S4A20 - adiciona schema e modulo de conexao com o cluster do mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    JogadoresModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:EReapH1DEeOwnRYQ@cluster0.bkfxfuz.mongodb.net/smartranking?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTipology: true,
+        useFindAndModify: false,
+      },
+    ),
+  ],
   controllers: [],
   providers: [],
 })
