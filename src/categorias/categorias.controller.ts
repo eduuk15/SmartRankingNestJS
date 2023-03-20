886723d8aac39cfe18534855a57ddb04364c2b26
Eduardo <eduardo@conectra.com.br>
2023-03-20 15:25:13 -0300

    S6A28 - adiciona metodo de consulta de categoria pelo id

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,6 +1,8 @@
 import {
   Body,
   Controller,
+  Get,
+  Param,
   Post,
   UsePipes,
   ValidationPipe,
@@ -19,5 +21,17 @@ export class CategoriasController {
     @Body() criarCategoriaDto: CriarCategoriaDto,
   ): Promise<Categoria> {
     return await this.categoriasService.criarCategoria(criarCategoriaDto);
+  }
+
+  @Get()
+  async consultarCategorias(): Promise<Array<Categoria>> {
+    return await this.categoriasService.consultarTodasCategorias();
+  }
+
+  @Get('/:categoria')
+  async consultarCategoriaPeloId(
+    @Param('categoria') categoria: string,
+  ): Promise<Categoria> {
+    return await this.categoriasService.consultarCategoriaPeloId(categoria);
   }
 }
